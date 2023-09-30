b6773ee6ea5aed3358d39cc56991fa5fd4be3494
AxeKam333 <olek.kaminski5@gmail.com>
2023-09-30 17:26:37 +0200

    home page require login, creating posts

--- main/views.py
+++ main/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render, redirect
-from .forms import RegisterForm
+from .forms import RegisterForm, PostForm
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, logout, authenticate
 
 # Create your views here.
 
+@login_required(login_url='/login')
 def home(request):
     return render(request, 'main/home.html')
+
+@login_required(login_url='/login')
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('/home')
+    else:
+        form = PostForm()
+
+    return render(request, 'main/create_post.html', {"form": form})
 
 def sign_up(request):
     if request.method == 'POST':
@@ -17,3 +33,4 @@ def sign_up(request):
     else:
         form = RegisterForm()
     return render(request, 'registration/sign_up.html', {"form": form})
+
